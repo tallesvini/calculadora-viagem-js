00bc76a9dd11c29f9ae10879451ad58020b6e90a
Talles Vinicius <tallesvinicius62@gmail.com>
2021-11-12 11:41:38 -0300

    Alteração de cores e adição de input

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let n1 = document.querySelector('#num1')
 let n2 = document.querySelector('#num2')
 let n3 = document.querySelector('#num3')
+let n4 = document.querySelector('#num4')
 
 let resultado1 = document.querySelector('#resultado-1')
 let resultado2 = document.querySelector('#resultado-2')
@@ -10,6 +11,7 @@ let resultado3 = document.querySelector('#resultado-3')
         const distância = parseFloat(n1.value)
         const velocidade = parseFloat(n2.value)
         const alcool = parseFloat(n3.value)
+        const km = parseFloat(n4.value)
 
         // tempo de viagem -------------
 
@@ -50,7 +52,7 @@ let resultado3 = document.querySelector('#resultado-3')
         let consumo = function(distância, médiaConsumo) {
             return distância / médiaConsumo;
         }
-        let combustível = consumo(distância, 14);
+        let combustível = consumo(distância, km);
         
         
         resultado2.innerHTML = combustível.toFixed(2) + ' Litros'
